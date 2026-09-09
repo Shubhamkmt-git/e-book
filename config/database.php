2f6232456937e3dccd04402ca9b58f59733e98f0
Shubhamkmt-git <shubham332709@gmail.com>
2026-09-09 14:47:02 +0530

    Fix database connection handling and enforce MySQL connection

--- config/database.php
+++ config/database.php
@@ -35,7 +35,7 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => str_ends_with((string) env('DB_DATABASE', ''), '.sqlite') ? database_path(env('DB_DATABASE')) : database_path('database.sqlite'),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
